10cb8d11dac5e126dbe303118917f2a2f545038c
songyi <song21677@naver.com>
2023-08-24 13:19:44 +0900

    feat: 가장 큰 수 구하기

--- song21677/1WEEK/src/main/java/onboarding/Problem1.java
+++ song21677/1WEEK/src/main/java/onboarding/Problem1.java
@@ -5,6 +5,10 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         if (!verifyInput(pobi, crong)) return -1;
+
+        int pobiMax = findMax(pobi);
+        int crongMax = findMax(crong);
+
         return 0;
     }
 
@@ -18,5 +22,29 @@ class Problem1 {
         if (pobi.get(1) != pobi.get(0)+1 || crong.get(1) != crong.get(0)+1) return false;
 
         return true;
+    }
+
+    private static int findMax(List<Integer> person) {
+        String leftPage = String.valueOf(person.get(0));
+        String rightPage = String.valueOf(person.get(1));
+
+        int leftMax = findPageMax(leftPage);
+        int rightMax = findPageMax(rightPage);
+
+        return leftMax > rightMax? leftMax : rightMax;
+    }
+
+    public static int findPageMax(String page) {
+        int sum = 0;
+        for (int i=0; i<page.length(); ++i) {
+            sum += Integer.parseInt(page.substring(i, i+1));
+        }
+
+        int multiply = 1;
+        for (int i=0; i<page.length(); ++i) {
+            multiply *= Integer.parseInt(page.substring(i, i+1));
+        }
+
+        return sum > multiply? sum : multiply;
     }
 }
