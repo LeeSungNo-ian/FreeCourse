5d18c5ef657ce98df9f4bf16a8c2aa3dadaf09dd
songyi <song21677@naver.com>
2023-08-24 13:17:31 +0900

    feat: 입력값 검증

--- song21677/1WEEK/src/main/java/onboarding/Problem1.java
+++ song21677/1WEEK/src/main/java/onboarding/Problem1.java
@@ -4,7 +4,19 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        if (!verifyInput(pobi, crong)) return -1;
+        return 0;
+    }
+
+    public static boolean verifyInput(List<Integer> pobi, List<Integer> crong) {
+        if (pobi.size() != 2 || crong.size() != 2) return false;
+
+        if (pobi.get(0) < 3 || pobi.get(0) > 397 || crong.get(0) < 3 || crong.get(0) > 397) return false;
+
+        if (pobi.get(0) % 2 == 0 || crong.get(0) % 2 == 0) return false;
+
+        if (pobi.get(1) != pobi.get(0)+1 || crong.get(1) != crong.get(0)+1) return false;
+
+        return true;
     }
 }
