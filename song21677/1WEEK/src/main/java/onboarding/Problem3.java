1d5b568f0f64e684dd95f29b2d0a71022787c0f5
songyi <song21677@naver.com>
2023-08-27 23:33:54 +0900

    feat: 3, 6, 9의 수만큼 박수 치기

--- song21677/1WEEK/src/main/java/onboarding/Problem3.java
+++ song21677/1WEEK/src/main/java/onboarding/Problem3.java
@@ -1,8 +1,14 @@
 package onboarding;
 
 public class Problem3 {
-    public static int solution(int number) {
-        int answer = 0;
-        return answer;
+    static int clap(int curr) {
+        int clap = 0;
+        String cur = String.valueOf(curr);
+        for (int i=0; i<cur.length(); i++) {
+            if (cur.charAt(i) == '3' || cur.charAt(i) == '6' || cur.charAt(i) == '9') {
+                clap++;
+            }
+        }
+        return clap;
     }
 }
