f48c5212dc20517cf2af3433bbc74cc789668934
LeeSungNo-ian <isnn0103@gmail.com>
2023-08-27 18:18:58 +0900

    feat: 각 자리수 덧셈, 곱셈 기능 구현 (Problem1)
    - 페이지 각 자리수의 합을 구합니다.
    - 페이지 각 자리수의 곱을 구합니다.

--- ian/WEEK1/WEEK1/src/Problem1.java
+++ ian/WEEK1/WEEK1/src/Problem1.java
@@ -1,2 +1,24 @@
 public class Problem1 {
+    static class Calculator {
+        public static int plus(int page) {
+            int result = 0;
+            while (page > 0) {
+                result += page % 10;
+                page /= 10;
+            }
+            return result;
+        }
+
+        public static int multiply(int page) {
+            int result = 1;
+            while (page > 0) {
+                result *= page % 10;
+                page /= 10;
+            }
+            return result;
+        }
+    }
+    public static void main(String[] args) {
+
+    }
 }
