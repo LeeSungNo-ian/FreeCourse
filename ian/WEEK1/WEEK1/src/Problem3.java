78140418186923bc7e4c340bad344ec4d84744f4
LeeSungNo-ian <isnn0103@gmail.com>
2023-08-27 20:08:12 +0900

    feat: 3, 6, 9가 포함된 개수 추출 기능 구현 (Problem2)
    - 1부터 주어진 숫자까지를 순회하며 3, 6, 9가 몇 번 들어갔는지 확인해줍니다.

--- ian/WEEK1/WEEK1/src/Problem3.java
+++ ian/WEEK1/WEEK1/src/Problem3.java
@@ -1,2 +1,18 @@
 public class Problem3 {
+    public static int solution(int number) {
+        int clapCount = 0;
+        for (int i = 1; i <= number; i++) {
+            int currentNumber = i;
+            while (currentNumber > 0) {
+                if (currentNumber % 10 == 3 || currentNumber % 10 == 6 || currentNumber % 10 == 9) {
+                    clapCount ++;
+                }
+                currentNumber /= 10;
+            }
+        }
+        return clapCount;
+    }
+
+    public static void main(String[] args) {
+    }
 }
