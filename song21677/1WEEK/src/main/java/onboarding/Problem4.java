fb96b8f6fccd5c8d6987b6ac044ba7d2535186a9
songyi <song21677@naver.com>
2023-08-27 23:56:03 +0900

    feat: [Problem4] 알파벳인지 확인하기

--- song21677/1WEEK/src/main/java/onboarding/Problem4.java
+++ song21677/1WEEK/src/main/java/onboarding/Problem4.java
@@ -4,8 +4,13 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Problem4 {
+
     private static boolean verifySize(String word) {
         return 1 <= word.length() && word.length() <= 1000;
+    }
+
+    private static boolean verifyChar(String c) {
+        return Character.isAlphabetic(c.charAt(0));
     }
 
 }
