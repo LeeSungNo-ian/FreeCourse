7b30fb3a8654df0991609c81897eb254de4047e0
songyi <song21677@naver.com>
2023-08-27 23:45:35 +0900

    feat: Problem3 solution

--- song21677/1WEEK/src/main/java/onboarding/Problem3.java
+++ song21677/1WEEK/src/main/java/onboarding/Problem3.java
@@ -2,6 +2,11 @@ package onboarding;
 
 public class Problem3 {
     static int sum = 0;
+    public static int solution(int number) {
+        int answer = game369(1, number);
+        return answer;
+    }
+
     static int game369(int curr, int number) {
         if (curr == number+1) return sum;
         sum += clap(curr);
