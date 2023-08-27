0ff83f274aed521231b8ed4d5af46ecaa6bafb93
songyi <song21677@naver.com>
2023-08-28 06:32:12 +0900

    feat: [Problem2] solution

--- song21677/1WEEK/src/main/java/onboarding/Problem2.java
+++ song21677/1WEEK/src/main/java/onboarding/Problem2.java
@@ -3,6 +3,14 @@ package onboarding;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+        int idx = cryptogram.length()-1;
+        while (true) {
+            String[] str = findDuplicatedString(cryptogram, idx, idx - 1, 0);
+            idx = Integer.parseInt(str[1]);
+            if (idx == -1) break;
+            cryptogram = str[0];
+        }
+        answer = cryptogram;
         return answer;
     }
 }
