7a20ab1785fdeb8f8e2c43a151551a7180980190
LeeSungNo-ian <isnn0103@gmail.com>
2023-08-27 20:56:42 +0900

    feat: 금액에 따라 생기는 배열 구현 (Problem5)

--- ian/WEEK1/WEEK1/src/Problem5.java
+++ ian/WEEK1/WEEK1/src/Problem5.java
@@ -1,2 +1,15 @@
 public class Problem5 {
+    public static int[] solution(int money) {
+        int[] coin = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        int[] result = new int[coin.length];
+
+        for (int i = 0; i < coin.length; i++) {
+            result[i] = money / coin[i];
+            money %= coin[i];
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+
+    }
 }
