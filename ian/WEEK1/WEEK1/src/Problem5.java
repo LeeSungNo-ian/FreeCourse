e1bb5f72a44cae094f2c49d273aaa0a204572f6b
LeeSungNo-ian <isnn0103@gmail.com>
2023-08-27 21:00:45 +0900

    feat: 입력 기능 구현 (Problem5)

--- ian/WEEK1/WEEK1/src/Problem5.java
+++ ian/WEEK1/WEEK1/src/Problem5.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Problem5 {
     public static int[] solution(int money) {
         int[] coin = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
@@ -10,6 +13,10 @@ public class Problem5 {
         return result;
     }
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int money = sc.nextInt();
 
+        int[] result = solution(money);
+        System.out.println(Arrays.toString(result));
     }
 }
