f57a8cd6a831556eb4251f3853de5befe37d2bcc
LeeSungNo-ian <isnn0103@gmail.com>
2023-08-27 20:11:12 +0900

    feat: 입력 기능 구현 (Problem3)
    - input 된 값에 따라 결과값이 출력됩니다.

--- ian/WEEK1/WEEK1/src/Problem3.java
+++ ian/WEEK1/WEEK1/src/Problem3.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Problem3 {
     public static int solution(int number) {
         int clapCount = 0;
@@ -15,5 +17,9 @@ public class Problem3 {
     }
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+
+        System.out.println(solution(number));
     }
 }
