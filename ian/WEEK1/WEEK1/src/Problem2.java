faa4498d7e6af22132f93afede18bbeb33e549b2
LeeSungNo-ian <isnn0103@gmail.com>
2023-08-27 19:47:22 +0900

    feat: 중복된 값 pop 기능 구현 (Problem2)
    - for문을 통해 순차적으로 조회된 char 중 중복된 값은 pop하며, 중복되지 않는 값은 push합니다.
    - result라는 StringBuilder 생성 후 값들을 추가합니다.

--- ian/WEEK1/WEEK1/src/Problem2.java
+++ ian/WEEK1/WEEK1/src/Problem2.java
@@ -1,2 +1,24 @@
+import java.util.Stack;
+
 public class Problem2 {
+    public String solution(String cryptogram) {
+        Stack<Character> stack = new Stack<>();
+        for (char ch: cryptogram.toCharArray()) {
+            if (!stack.isEmpty() && stack.peek() == ch) {
+                stack.pop();
+            } else {
+                stack.push(ch);
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+        for (char ch: stack) {
+            result.append(ch);
+        }
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
