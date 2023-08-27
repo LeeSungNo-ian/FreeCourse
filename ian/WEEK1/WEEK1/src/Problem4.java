e1e462b4104b8ee6385962f4f8d464de804584d7
LeeSungNo-ian <isnn0103@gmail.com>
2023-08-27 20:35:45 +0900

    feat: 문자열 변환 기능 구현 (Problem4)
    - 각 char 별 고유 유니코드를 이용하여 변환하는 작업을 완료했습니다.

--- ian/WEEK1/WEEK1/src/Problem4.java
+++ ian/WEEK1/WEEK1/src/Problem4.java
@@ -1,2 +1,17 @@
+import java.util.Scanner;
+
 public class Problem4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if ('A' <= ch && ch <= 'Z') {
+                System.out.print((char)(155 - ch));
+            } else {
+                System.out.print((char)(219 - ch));
+            }
+        }
+    }
 }
