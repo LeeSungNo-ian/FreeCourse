8edb90ab154b68b0b76aace6911bada8c36a7ee3
Minho Kim <music4537@naver.com>
2023-08-27 16:45:19 +0900

    Test : 게임 테스트 코드 구현 완료

--- MinhoKim/1주차/onboarding/src/test/java/Week1/onboarding/Problem3Test.java
+++ MinhoKim/1주차/onboarding/src/test/java/Week1/onboarding/Problem3Test.java
@@ -11,4 +11,11 @@ public class Problem3Test {
         int result = 4;
         assertThat(Problem3.game.start(number)).isEqualTo(result);
     }
+
+    @Test
+    public void gameTest2() {
+        int number = 324;
+        int result = 212;
+        assertThat(Problem3.game.start(number)).isEqualTo(result);
+    }
 }
