a193dd15d821f64db8c4310ade541c1bad308813
LeeSungNo-ian <isnn0103@gmail.com>
2023-08-27 19:49:58 +0900

    feat: 입력 기능 구현 (Problem2)
    - input 된 값에 따라 결과값이 출력됩니다.

--- ian/WEEK1/WEEK1/src/Problem2.java
+++ ian/WEEK1/WEEK1/src/Problem2.java
@@ -1,7 +1,8 @@
 import java.util.Stack;
+import java.util.Scanner;
 
 public class Problem2 {
-    public String solution(String cryptogram) {
+    static public String solution(String cryptogram) {
         Stack<Character> stack = new Stack<>();
         for (char ch: cryptogram.toCharArray()) {
             if (!stack.isEmpty() && stack.peek() == ch) {
@@ -19,6 +20,9 @@ public class Problem2 {
     }
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String cryptogram = sc.nextLine();
 
+        System.out.println(solution(cryptogram));
     }
 }
