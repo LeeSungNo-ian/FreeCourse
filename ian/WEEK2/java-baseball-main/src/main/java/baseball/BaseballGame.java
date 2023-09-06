381ace142afd98328ef1f606e4ab6c45e07d462d
LeeSungNo-ian <isnn0103@gmail.com>
2023-09-06 23:39:52 +0900

    feat: 게임 지속 여부 확인 기능 구현

--- ian/WEEK2/java-baseball-main/src/main/java/baseball/BaseballGame.java
+++ ian/WEEK2/java-baseball-main/src/main/java/baseball/BaseballGame.java
@@ -7,6 +7,7 @@ import static utils.PlayerInputUtil.perceiveBallNumber;
 import static utils.PlayerGameRestartUtil.gameRestartQuestion;
 
 public class BaseballGame {
+
     private Computer computer = new Computer();
     private Player player = new Player();
     private Referee referee = new Referee();
@@ -15,7 +16,15 @@ public class BaseballGame {
     private Set<Integer> computerBalls;
     private boolean isFirstRound = true;
 
+    public static final int GAME_ONGOING = 0;
+    public static final int CONTINUE_GAME = 1;
+    public static final int FINISH_GAME = 2;
+
+    int gameStatus = GAME_ONGOING;
+
     public void gameStart() {
+        gameStatus = GAME_ONGOING;
+
         while (true) {
             if (isFirstRound) {
                 System.out.println("숫자 야구 게임을 시작합니다.");
@@ -23,6 +32,7 @@ public class BaseballGame {
                 computer.throwThreeUniqueBalls();
                 computerBalls = computer.getComputerNumber();
                 isFirstRound = false;
+                System.out.println(computerBalls);
             }
             System.out.print("숫자를 입력해주세요 : ");
 
@@ -33,9 +43,28 @@ public class BaseballGame {
             System.out.println(gameResult);
 
             if (gameResult.equals("3스트라이크")) {
-                System.out.println("3스트라이크 일 때 상황을 가정하는 함수입니다 :)️");
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n️");
+                decideAdditionalGame();
+            }
+
+            if (gameStatus == FINISH_GAME) {
+                break;
+            }
+
+            if (gameStatus == CONTINUE_GAME) {
+                isFirstRound = true;
+                gameStatus = GAME_ONGOING;
+                System.out.println("게임 재시작");
             }
         }
+    }
+
+    private void decideAdditionalGame() {
+        String nextGameState = gameRestartQuestion();
+        if (nextGameState.equals("1")) {
+            gameStatus = CONTINUE_GAME;
+        }
+        gameStatus = FINISH_GAME;
     }
 }
 
