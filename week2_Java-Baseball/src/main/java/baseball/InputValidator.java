dec1a236bdb67f7d2c59ce48123b20a48543d202
seoyoung1013 <lemi01@naver.com>
2023-09-11 16:05:46 +0900

    입력값 유효성 검사

--- week2_Java-Baseball/src/main/java/baseball/InputValidator.java
+++ week2_Java-Baseball/src/main/java/baseball/InputValidator.java
@@ -2,6 +2,7 @@ package baseball;
 import java.util.Scanner;
 
 public class InputValidator {
+
     public static int[] getUserInput() {
         int[] numbers = new int[3];
         Scanner scanner = new Scanner(System.in);
@@ -10,17 +11,9 @@ public class InputValidator {
             try {
                 System.out.print("서로 다른 3자리의 숫자를 입력해주세요: ");
                 String input = scanner.next();
-                if (input.length() != 3) {
-                    throw new IllegalArgumentException("입력한 숫자는 3자리여야 합니다.");
-                }
-
-                for (int i = 0; i < 3; i++) {
-                    numbers[i] = Integer.parseInt(input.substring(i, i + 1));
-                }
-
-                if (numbers[0] == numbers[1] || numbers[0] == numbers[2] || numbers[1] == numbers[2]) {
-                    throw new IllegalArgumentException("서로 다른 숫자를 입력해주세요.");
-                }
+                validateInputLength(input);
+                convertInputToNumbers(input, numbers);
+                validateDistinctNumbers(numbers);
 
                 break;
             } catch (NumberFormatException e) {
@@ -31,5 +24,23 @@ public class InputValidator {
         }
 
         return numbers;
+    }
+
+    private static void validateInputLength(String input) {
+        if (input.length() != 3) {
+            throw new IllegalArgumentException("입력한 숫자는 3자리여야 합니다.");
+        }
+    }
+
+    private static void convertInputToNumbers(String input, int[] numbers) {
+        for (int i = 0; i < 3; i++) {
+            numbers[i] = Integer.parseInt(input.substring(i, i + 1));
+        }
+    }
+
+    private static void validateDistinctNumbers(int[] numbers) {
+        if (numbers[0] == numbers[1] || numbers[0] == numbers[2] || numbers[1] == numbers[2]) {
+            throw new IllegalArgumentException("서로 다른 숫자를 입력해주세요.");
+        }
     }
 }
