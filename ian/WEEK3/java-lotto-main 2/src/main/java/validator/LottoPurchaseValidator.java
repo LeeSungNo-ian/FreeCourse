e0502b9a6d29639ed8b25833711026a01536e301
LeeSungNo-ian <isnn0103@gmail.com>
2023-09-14 20:12:39 +0900

    feat(LottoPurchaseValidator): 입력금액이 정수로 이루어졌는지 확인

--- ian/WEEK3/java-lotto-main 2/src/main/java/validator/LottoPurchaseValidator.java
+++ ian/WEEK3/java-lotto-main 2/src/main/java/validator/LottoPurchaseValidator.java
@@ -2,6 +2,13 @@ package validator;
 
 public class LottoPurchaseValidator {
 
+    void vaildIntegerPurchasAmout(String input) {
+        String regex = "[+-]?\\d*(\\.\\d+)?";
+        if (!input.matches(regex)) {
+            throw new IllegalArgumentException("[ERROR] 로또 구입 금액에는 숫자만 입력해주세요.");
+        }
+    }
+
     void validMinimumPurchaseAmount(int purchaseAmount) {
         if (purchaseAmount < 1000) {
             throw new IllegalArgumentException("[ERROR] 로또 최소 구입금액은 1000원 입니다.");
