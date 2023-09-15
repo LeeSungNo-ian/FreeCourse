98299835a2022eac49d6aa0a8ff7455c95980801
JiHyeonL <lejinhy@gmail.com>
2023-09-15 21:53:01 +0900

    feat: lotto 도메인 완성

--- JiHyeonL/3WEEK/java-lotto-main/src/main/java/lotto/domain/Lotto.java
+++ JiHyeonL/3WEEK/java-lotto-main/src/main/java/lotto/domain/Lotto.java
@@ -1,18 +1,19 @@
-package lotto;
+package lotto.domain;
 
 import java.util.List;
 
+/**
+ * 당첨 번호 엔티티.
+ * 참고) domain은 setter 사용 x
+ */
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
-
+    public List<Integer> getLotto() {
+        return numbers;
     }
-
-    // TODO: 추가 기능 구현
 }
