9f6227883c697f918db45a9af17bccb8ef437a7f
JiHyeonL <lejinhy@gmail.com>
2023-09-15 22:17:07 +0900

    feat: 로또 생성 기능 완성

--- JiHyeonL/3WEEK/java-lotto-main/src/main/java/lotto/service/MakeLotto.java
+++ JiHyeonL/3WEEK/java-lotto-main/src/main/java/lotto/service/MakeLotto.java
@@ -11,7 +11,14 @@ public class MakeLotto {
     /*
      * 랜덤 로또 발행하기
      */
-    public List<Integer> generatedRandomNumbers() {
+    public List<Integer> randomNumbers() {
         return Randoms.pickUniqueNumbersInRange(Info.START_NUMBER.getValue(), Info.END_NUMBER.getValue(), Info.COUNT_NUMBERS.getValue());
     }
+    /*
+     * 로또 몇개 구매했는지 계산
+     */
+    public int quantity(int cost) {
+        return cost / Info.LOTTO_UNIT.getValue();
+    }
+
 }
