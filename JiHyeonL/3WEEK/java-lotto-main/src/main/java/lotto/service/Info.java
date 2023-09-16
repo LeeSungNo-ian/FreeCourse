1d2bd472028c8442560573f0fe8cb58a9c6c770e
JiHyeonL <lejinhy@gmail.com>
2023-09-16 16:40:37 +0900

    refactor: info enum에 순위 개수 추가

--- JiHyeonL/3WEEK/java-lotto-main/src/main/java/lotto/service/Info.java
+++ JiHyeonL/3WEEK/java-lotto-main/src/main/java/lotto/service/Info.java
@@ -4,7 +4,8 @@ public enum Info {
     LOTTO_UNIT(1000), //로또 한 장 가격
     COUNT_NUMBERS(6),   // 로또 한 장당 번호 개수
     START_NUMBER(1),    // 로또 가능한 최소 숫자
-    END_NUMBER(45);     // 로또 가능한 최대 숫자
+    END_NUMBER(45),     // 로또 가능한 최대 숫자
+    RANK_LENGTH(5); // 순위 개수
     private int value;
 
     Info(int value) {
