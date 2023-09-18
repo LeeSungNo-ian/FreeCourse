66151390efa95aad24f5fd0c62970760e89e4548
LeeSungNo-ian <isnn0103@gmail.com>
2023-09-18 23:34:16 +0900

    feat(RandomNumberGeneratorValidator): 난수 중복 유효성 판단

--- ian/WEEK3/java-lotto-main 2/src/main/java/validator/RandomNumberGeneratorValidator.java
+++ ian/WEEK3/java-lotto-main 2/src/main/java/validator/RandomNumberGeneratorValidator.java
@@ -1,11 +1,14 @@
 package validator;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class RandomNumberGeneratorValidator {
     public void vaildRandomNumbers(List<Integer> numbers) {
         vaildRandomNumberRange(numbers);
         vaildRandomNumberSize(numbers);
+        vaildRandomNumberDuplication(numbers);
     }
 
     void vaildRandomNumberRange(List<Integer> randomNumbers) {
@@ -23,6 +26,13 @@ public class RandomNumberGeneratorValidator {
     void vaildRandomNumberSize(List<Integer> randomNumbers) {
         if (randomNumbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 난수가 6개로 생성되지 않았습니다.");
+        }
+    }
+
+    void vaildRandomNumberDuplication(List<Integer> randomNumbers) {
+        Set<Integer> uniqueNumbers = new HashSet<>(randomNumbers);
+        if (uniqueNumbers.size() != randomNumbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 난수 중 중복된 값이 있습니다.");
         }
     }
 }
